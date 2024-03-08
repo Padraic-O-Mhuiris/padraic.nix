fe28bce54dfa61ff279a5a5ceca13ac9664e509f
Padraic-O-Mhuiris <patrick.morris.310@gmail.com>
2024-03-08 14:51:52 +0000

    Ensure home-manager xsession script path is referenced

--- home/windowManager/i3/default.nix
+++ home/windowManager/i3/default.nix
@@ -5,32 +5,36 @@ in {
 
   imports = [ ../../launcher/rofi.nix ../../terminal/alacritty.nix ];
 
-  xsession.windowManager.i3 = {
+  xsession = {
     enable = true;
-    config = {
-      inherit modifier;
-      terminal = config.home.sessionVariables.TERMINAL;
-      menu = config.home.sessionVariables.LAUNCHER;
-      keybindings = l.mkOptionDefault {
-        "${modifier}+Shift+q" = null;
-        "${modifier}+Return" =
-          "exec ${config.xsession.windowManager.i3.config.terminal}";
-        "${modifier}+q" = "kill";
+    scriptPath = ".hm-xsession";
+    windowManager.i3 = {
+      enable = true;
+      config = {
+        inherit modifier;
+        terminal = config.home.sessionVariables.TERMINAL;
+        menu = config.home.sessionVariables.LAUNCHER;
+        keybindings = l.mkOptionDefault {
+          "${modifier}+Shift+q" = null;
+          "${modifier}+Return" =
+            "exec ${config.xsession.windowManager.i3.config.terminal}";
+          "${modifier}+q" = "kill";
+        };
+        defaultWorkspace = "workspace number 1";
+        gaps = {
+          inner = 10;
+          outer = 5;
+        };
+        floating = {
+          titlebar = false;
+          border = 1;
+        };
+        window = {
+          titlebar = false;
+          border = 1;
+        };
+        # bars = [ ];
       };
-      defaultWorkspace = "workspace number 1";
-      gaps = {
-        inner = 10;
-        outer = 5;
-      };
-      floating = {
-        titlebar = false;
-        border = 1;
-      };
-      window = {
-        titlebar = false;
-        border = 1;
-      };
-      # bars = [ ];
     };
   };
 }
