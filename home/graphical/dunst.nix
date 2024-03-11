6ae425f7dc10de4f1bd1233149bf1102aa40204f
Padraic-O-Mhuiris <patrick.morris.310@gmail.com>
2024-03-11 19:06:05 +0000

    Added dunst

---
+++ home/graphical/dunst.nix
@@ -0,0 +1,34 @@
+{ config, pkgs, ... }:
+
+let inherit (config.stylix.fonts) sansSerif sizes;
+in {
+  services.dunst = {
+    enable = true;
+    iconTheme = {
+      name = "Adwaita";
+      package = pkgs.gnome3.adwaita-icon-theme;
+    };
+    settings = {
+      global = {
+        monitor = 0;
+        width = 400;
+        height = 300;
+        offset = "10x10";
+        scale = 0;
+        follow = "mouse";
+        origin = "top-right";
+        shrink = "yes";
+        padding = 16;
+        horizontal_padding = 16;
+        font = "${sansSerif.name} ${toString sizes.applications}";
+        # separator_color = lib.os.colors.silver;
+        line_height = 4;
+        format = "<b>%s</b>\\n%b";
+        corner_radius = 5;
+        icon_corner_radius = 5;
+        min_icon_size = 128;
+        max_icon_size = 128;
+      };
+    };
+  };
+}
--- home/graphical/i3/default.nix
+++ home/graphical/i3/default.nix
@@ -3,8 +3,14 @@
 let modifier = "Mod4";
 in {
 
-  imports =
-    [ ../xsession.nix ../rofi.nix ../alacritty.nix ../fonts.nix ../stylix.nix ];
+  imports = [
+    ../xsession.nix
+    ../rofi.nix
+    ../alacritty.nix
+    ../fonts.nix
+    ../stylix.nix
+    ../dunst.nix
+  ];
 
   xsession.windowManager.i3 = {
     enable = true;
