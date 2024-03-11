540e6bcab5fd75a8486cab08e96cd6405c6d9d51
Padraic-O-Mhuiris <patrick.morris.310@gmail.com>
2024-03-11 17:37:21 +0000

    fix stylix image

--- system/graphical/stylix.nix
+++ system/graphical/stylix.nix
@@ -1,16 +1,15 @@
 { inputs, pkgs, ... }:
 
-let
-  base16Scheme = "${pkgs.base16-schemes}/share/themes/nord.yaml";
-  image = pkgs.runCommand "image.png" { } ''
-    COLOR=$(${pkgs.yq}/bin/yq -r .base00 ${base16Scheme})
-    COLOR="#"$COLOR
-    ${pkgs.imagemagick}/bin/magick convert -size 1920x1080 xc:$COLOR $out'';
+let base16Scheme = "${pkgs.base16-schemes}/share/themes/nord.yaml";
 in {
   imports = [ inputs.stylix.nixosModules.stylix ];
-
   stylix = {
-    inherit base16Scheme image;
+    inherit base16Scheme;
+    image = pkgs.fetchurl {
+      url =
+        "https://www.pixelstalk.net/wp-content/uploads/2016/05/Epic-Anime-Awesome-Wallpapers.jpg";
+      sha256 = "enQo3wqhgf0FEPHj2coOCvo7DuZv+x5rL/WIo4qPI50=";
+    };
     autoEnable = false;
     homeManagerIntegration = {
       autoImport = true;
