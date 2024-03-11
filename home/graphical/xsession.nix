d22ae85395e033262db9f49ba4ffae836620fb58
Padraic-O-Mhuiris <patrick.morris.310@gmail.com>
2024-03-11 16:16:04 +0000

    Extended xsession profile to launch autorandr

--- home/graphical/xsession.nix
+++ home/graphical/xsession.nix
@@ -1,6 +1,14 @@
+{ l, pkgs, nixosConfig, ... }:
+
 {
   xsession = {
     enable = true;
     scriptPath = ".hm-xsession";
+    profileExtra = if nixosConfig.services.autorandr.enable then
+      "${
+        l.getExe' pkgs.autorandr "autorandr"
+      } -l ${nixosConfig.services.autorandr.defaultTarget}"
+    else
+      "";
   };
 }
