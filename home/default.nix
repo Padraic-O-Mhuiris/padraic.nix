f693c982082b07adfec11cc205c3ab661aa40822
Padraic-O-Mhuiris <patrick.morris.310@gmail.com>
2024-03-11 13:32:12 +0000

    Added user dir layout

--- home/default.nix
+++ home/default.nix
@@ -1,12 +1,28 @@
-{ stateVersion, pkgs, ... }: {
+{ stateVersion, pkgs, config, ... }: {
 
   imports = [ ./programs ];
 
   home = {
-    packages = with pkgs; [ htop ];
+    packages = with pkgs; [
+      htop
+      # TODO Move this somewhere
+      gnome.nautilus
+    ];
 
     enableNixpkgsReleaseCheck = true;
     inherit stateVersion;
+  };
+
+  xdg = {
+    enable = true;
+    userDirs = {
+      enable = true;
+      createDirectories = true;
+      desktop = "${config.home.homeDirectory}/desktop";
+      download = "${config.home.homeDirectory}/downloads";
+      extraConfig = { XDG_CODE_DIR = "${config.home.homeDirectory}/code"; };
+      music = "${config.home.homeDirectory}/music";
+    };
   };
 
   systemd.user.startServices = "sd-switch";
