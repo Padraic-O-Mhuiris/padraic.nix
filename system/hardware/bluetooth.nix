e44f4452a192851e8a44872a8db66e2e8cfb3a2a
Padraic-O-Mhuiris <patrick.morris.310@gmail.com>
2024-03-11 18:49:13 +0000

    Update bluetooth configuration

--- system/hardware/bluetooth.nix
+++ system/hardware/bluetooth.nix
@@ -1,9 +1,22 @@
-_:
+{ pkgs, ... }:
 
+# https://discourse.nixos.org/t/bluetooth-troubles/38940/11
 {
   hardware.bluetooth = {
     enable = true;
-    settings = { General.Enable = "Source,Sink,Media,Socket"; };
+    powerOnBoot = true;
+    package = pkgs.bluez5-experimental;
+    settings = {
+      General.Enable = "Source,Sink,Media,Socket";
+      Policy.AutoEnable = "true";
+    };
   };
   services.blueman.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    bluez5-experimental
+    bluez-tools
+    bluez-alsa
+    bluetuith
+  ];
 }
