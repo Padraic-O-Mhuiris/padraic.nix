f3246880be28f04da604e9a4d8039706b521f591
Padraic-O-Mhuiris <patrick.morris.310@gmail.com>
2024-08-06 13:25:43 +0100

    enabled atuin daemon

--- home/shell/zsh/atuin.nix
+++ home/shell/zsh/atuin.nix
@@ -1,16 +1,27 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 
 {
   programs.atuin = {
     enable = true;
     enableZshIntegration = true;
     settings = {
+      daemon.enabled = true;
       update_check = false;
       sync_address = "https://api.atuin.sh"; # TODO Change
       sync_frequency = "15m";
+      sync.records = true;
       dialect = "uk";
       key_path = config.sops.secrets.atuin_key.path;
     };
+  };
+
+  systemd.user.services.atuind = {
+    Unit = {
+      Description = "Initialises the atuin daemon";
+      After = "network.target";
+    };
+    Install = { WantedBy = [ "default.target" ]; };
+    Service = { ExecStart = "${pkgs.atuin}/bin/atuin daemon"; };
   };
 
   sops.secrets.atuin_key = { };
