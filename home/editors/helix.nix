2a784d25f721d68e387cedd6ff38682fa4cb4475
Padraic-O-Mhuiris <patrick.morris.310@gmail.com>
2024-09-25 14:09:37 +0100

    always ignore nix store paths and .direnv paths

--- home/editors/helix.nix
+++ home/editors/helix.nix
@@ -26,6 +26,10 @@
         "C-g" = ":sh tmux popup -d \"#{pane_current_path}\" -xC -yC -w80% -h80% -E ${pkgs.lazygit}/bin/lazygit";
       };
     };
+    ignores = [
+      "/nix/store/*"
+      ".direnv"
+    ];
     languages.language = [
       {
         name = "nix";
